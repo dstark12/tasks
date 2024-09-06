1ca0a187c0984f2f81d0968236821754b76b4796
Dustin Stark <dstark@udel.edu>
2024-09-06 12:01:25 -0400

    task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
+            <h1>
+                <header className="App-header">
+                    UD CISC275 with React Hooks and TypeScript
+                </header>
+            </h1>
+            <img src="/Users/dustin/Desktop/IMG_1790.jpeg" alt="A bird" />
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <ol>
+                <li>C</li>
+                <li>B</li>
+                <li>C</li>
+            </ol>
             <p>Dustin Stark</p>
-            <body>Hello World</body>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: 100,
+                                height: 50,
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                    <Col></Col>
+                </Row>
+            </Container>
         </div>
     );
 }
