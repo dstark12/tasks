9dbf6dd43ce7de03343c33964f74b883d65487a1
Dustin Stark <dstark@udel.edu>
2024-09-17 17:53:26 -0400

    finished objects

--- src/objects.ts
+++ src/objects.ts
@@ -8,9 +8,18 @@ import { Question, QuestionType } from "./interfaces/question";
 export function makeBlankQuestion(
     id: number,
     name: string,
-    type: QuestionType
+    type: QuestionType,
 ): Question {
-    return {};
+    return {
+        id,
+        name,
+        body: "",
+        type,
+        options: [],
+        expected: "",
+        points: 1,
+        published: false,
+    };
 }
 
 /**
@@ -21,6 +30,12 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
+    answer = answer.trim();
+    answer = answer.toLowerCase();
+    const expected = question.expected.toLowerCase();
+    if (expected === answer) {
+        return true;
+    }
     return false;
 }
 
@@ -31,7 +46,12 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "multiple_choice_question") {
+        if (!question.options.includes(answer)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 /**
@@ -41,7 +61,8 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const newQ = { ...question };
+    return newQ.id + ": " + newQ.name.slice(0, 10);
 }
 
 /**
@@ -62,7 +83,21 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    if (question.type === "short_answer_question") {
+        return "# " + question.name + "\n" + question.body;
+    }
+    return (
+        "# " +
+        question.name +
+        "\n" +
+        question.body +
+        "\n- " +
+        question.options[0] +
+        "\n- " +
+        question.options[1] +
+        "\n- " +
+        question.options[2]
+    );
 }
 
 /**
@@ -70,7 +105,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    return { ...question, name: newName };
 }
 
 /**
@@ -79,7 +114,7 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return { ...question, published: !question.published };
 }
 
 /**
@@ -89,7 +124,13 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    const newQuestion = {
+        ...oldQuestion,
+        id,
+        name: "Copy of " + oldQuestion.name,
+        published: false,
+    };
+    return newQuestion;
 }
 
 /**
@@ -100,7 +141,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const newQuestion = {
+        ...question,
+        options: [...question.options, newOption],
+    };
+    return newQuestion;
 }
 
 /**
@@ -115,7 +160,14 @@ export function mergeQuestion(
     id: number,
     name: string,
     contentQuestion: Question,
-    { points }: { points: number }
+    { points }: { points: number },
 ): Question {
-    return contentQuestion;
+    const merged = {
+        ...contentQuestion,
+        id,
+        name,
+        points: points,
+        published: false,
+    };
+    return merged;
 }
